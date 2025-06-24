407b6a39065a0b8ef94e29b232285ab5b117593f
Baruch Lubinsky <developer@karmadrivendevelopment.org>
2025-06-24 16:10:11 +0200

    deps

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kdd.MixProject do
   def project do
     [
       app: :kdd,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
@@ -32,16 +32,16 @@ defmodule Kdd.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.7.1"},
-      {:phoenix_ecto, "~> 4.4"},
-      {:ecto_sql, "~> 3.6"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 3.3"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.18.16"},
-      {:floki, ">= 0.30.0", only: :test},
-      {:phoenix_live_dashboard, "~> 0.7.2"},
-      {:esbuild, "~> 0.5", runtime: Mix.env() == :dev},
+      {:phoenix, "~> 1.7.21"},
+      {:phoenix_ecto, "~> 4.6.5"},
+      {:ecto_sql, "~> 3.13.2"},
+      {:postgrex, "~> 0.20.0"},
+      {:phoenix_html, "~> 4.2.1"},
+      {:phoenix_live_reload, "~> 1.6.0", only: :dev},
+      {:phoenix_live_view, "~> 1.0.17"},
+      {:floki, "~> 0.38.0", only: :test},
+      {:phoenix_live_dashboard, "~> 0.8.7"},
+      {:esbuild, "~> 0.10", runtime: Mix.env() == :dev},
       {:tailwind, "~> 0.1.8", runtime: Mix.env() == :dev},
       {:swoosh, "~> 1.3"},
       {:finch, "~> 0.13"},
